0c40203c0c0f8add80e9f14377f61d8f64cf1af3
zainab7681051 <zainab7681051@gmail.com>
2023-05-13 17:19:17 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,174 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { RouterLink, RouterView } from "vue-router";
+export default {
+  mounted() {
+    const toggle = document.querySelector(".toggle");
+    const menu = document.querySelector(".menu");
+    toggle.addEventListener("click", () => {
+      toggle.classList.toggle("active");
+      menu.classList.toggle("active");
+    });
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <header class="container">
+    <span class="logo">
+      <RouterLink to="/"><h1>SimpleFreeTube</h1></RouterLink>
+    </span>
+    <span class="toggle">
+      <span></span>
+      <span></span>
+    </span>
+    <div class="menu">
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+        <li><RouterLink to="/trending">Trending</RouterLink></li>
+      </ul>
     </div>
   </header>
-
   <RouterView />
 </template>
 
 <style scoped>
+.container {
+  padding-left: var(--containerXPadding);
+  padding-right: var(--containerXPadding);
+}
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  position: fixed;
+  z-index: 50;
+  width: 100%;
+  top: 0;
+  padding-top: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid var(--grey);
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  width: 4rem;
 }
 
-nav {
+.logo h1 {
+  color: var(--white);
+}
+
+.toggle {
+  width: 2rem;
+  height: 1.5rem;
+  cursor: pointer;
+  position: relative;
+}
+
+.toggle span {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: block;
+  border-bottom: 2px solid var(--white);
+  position: absolute;
+  transition: top 0.3s ease, transform 0.3s ease;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.toggle span:first-child {
+  top: 0.4rem;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.toggle span:last-child {
+  top: 0.9rem;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.toggle.active span {
+  top: 0.75rem;
 }
 
-nav a:first-of-type {
-  border: 0;
+.toggle.active span:first-child {
+  transform: rotate(45deg);
+}
+
+.toggle.active span:last-child {
+  transform: rotate(-45deg);
+}
+
+.menu {
+  position: fixed;
+  top: 5rem;
+  right: -15rem;
+  width: 15rem;
+  background: var(--darkgrey);
+  opacity: 0;
+  padding: 1rem;
+  transition: right 0.3s ease, opacity 0.3s ease;
+}
+
+.menu ul {
+  list-style: none;
+}
+
+.menu ul li {
+  text-transform: uppercase;
+  font-weight: 300;
+}
+
+.menu ul li > * {
+  color: var(--white);
+  display: block;
+  padding: 0.5rem;
+  letter-spacing: 0.1rem;
+  transition: color 0.3s ease;
+}
+
+.menu ul li > *:hover {
+  color: var(--blue);
+}
+
+.menu ul li + li {
+  border-top: 1px solid var(--grey);
+}
+
+.menu ul li > *.active {
+  color: var(--orange);
+  pointer-events: none;
+}
+
+.menu.active {
+  right: var(--containerXPadding);
+  opacity: 1;
 }
 
 @media (min-width: 1024px) {
-  header {
+  .toggle {
+    display: none;
+  }
+  .menu {
+    background: transparent;
+    position: static;
+    top: auto;
+    right: auto;
+    opacity: 1;
+    width: auto;
+    padding: 0;
+  }
+  .menu ul {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    gap: 3rem;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .menu ul li > * {
+    color: var(--white);
+    padding: 0;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .menu ul li + li {
+    border-top: none;
   }
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+a {
+  text-decoration: none;
 }
 </style>
